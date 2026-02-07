fb1b23629fdfaf73772a7584c2c88e6cd8714a14
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-02-07 21:29:24 +0100

    Updated linenoise.h to my changed version

--- src/linenoise.h
+++ src/linenoise.h
@@ -43,29 +43,26 @@
 extern "C" {
 #endif
 
-#include <stddef.h> /* For size_t. */
+#include <stddef.h> 
 
 extern char *linenoiseEditMore;
 
-/* The linenoiseState structure represents the state during line editing.
- * We pass this state to functions implementing specific editing
- * functionalities. */
+
 struct linenoiseState {
-    int in_completion;  /* The user pressed TAB and we are now in completion
-                         * mode, so input is handled by completeLine(). */
-    size_t completion_idx; /* Index of next completion to propose. */
-    int ifd;            /* Terminal stdin file descriptor. */
-    int ofd;            /* Terminal stdout file descriptor. */
-    char *buf;          /* Edited line buffer. */
-    size_t buflen;      /* Edited line buffer size. */
-    const char *prompt; /* Prompt to display. */
-    size_t plen;        /* Prompt length. */
-    size_t pos;         /* Current cursor position. */
-    size_t oldpos;      /* Previous refresh cursor position. */
-    size_t len;         /* Current edited line length. */
-    size_t cols;        /* Number of columns in terminal. */
-    size_t oldrows;     /* Rows used by last refrehsed line (multiline mode) */
-    int history_index;  /* The history index we are currently editing. */
+    int in_completion;  
+    size_t completion_idx; 
+    int ifd;            
+    int ofd;            
+    char *buf;          
+    size_t buflen;      
+    const char *prompt; 
+    size_t plen;        
+    size_t pos;         
+    size_t oldpos;      
+    size_t len;         
+    size_t cols;        
+    size_t oldrows;     
+    int history_index;  
 };
 
 typedef struct linenoiseCompletions {
@@ -73,18 +70,18 @@ typedef struct linenoiseCompletions {
   char **cvec;
 } linenoiseCompletions;
 
-/* Non blocking API. */
+
 int linenoiseEditStart(struct linenoiseState *l, int stdin_fd, int stdout_fd, char *buf, size_t buflen, const char *prompt);
 char *linenoiseEditFeed(struct linenoiseState *l);
 void linenoiseEditStop(struct linenoiseState *l);
 void linenoiseHide(struct linenoiseState *l);
 void linenoiseShow(struct linenoiseState *l);
 
-/* Blocking API. */
+
 char *linenoise(const char *prompt);
 void linenoiseFree(void *ptr);
 
-/* Completion API. */
+
 typedef void(linenoiseCompletionCallback)(const char *, linenoiseCompletions *);
 typedef char*(linenoiseHintsCallback)(const char *, int *color, int *bold);
 typedef void(linenoiseFreeHintsCallback)(void *);
@@ -93,13 +90,13 @@ void linenoiseSetHintsCallback(linenoiseHintsCallback *);
 void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback *);
 void linenoiseAddCompletion(linenoiseCompletions *, const char *);
 
-/* History API. */
+
 int linenoiseHistoryAdd(const char *line);
 int linenoiseHistorySetMaxLen(int len);
 int linenoiseHistorySave(const char *filename);
 int linenoiseHistoryLoad(const char *filename);
 
-/* Other utilities. */
+
 void linenoiseClearScreen(void);
 void linenoiseSetMultiLine(int ml);
 void linenoisePrintKeyCodes(void);
@@ -110,4 +107,4 @@ void linenoiseMaskModeDisable(void);
 }
 #endif
 
-#endif /* __LINENOISE_H */
+#endif 
