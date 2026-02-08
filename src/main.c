bf60439099d72c7e87cd93a7c43140fcd4ede212
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-02-08 07:38:00 +0100

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -137,7 +137,7 @@ int main(int argc, char *argv[]) {
          strcmp(argv[1], "-version") == 0)) {
 
         printf("CVX Shell beta 0.8.5\n");
-        printf("Copyright (C) 2025 JHX Studio's\n");
+        printf("Copyright (C) 2025-2026 JHX Studio's\n");
         printf("License: Elasna Open Source License v2\n");
         return 0;
     }
