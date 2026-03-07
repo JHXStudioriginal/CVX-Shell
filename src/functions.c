b998f48f624133270b1c97e93f9c495b3adfb971
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-03-07 23:02:21 +0100

    Updated functions.c

--- src/functions.c
+++ src/functions.c
@@ -1,3 +1,9 @@
+// Copyright (c) 2025-2026 JHXStudioriginal
+// This file is part of the Elasna Open Source License v3.
+// All original author information and file headers must be preserved.
+// For full license text, see: [https://github.com/JHXStudioriginal/Elasna-License/blob/main/LICENSE]
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
