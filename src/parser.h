cf9e932a0119f3dfd2988a3b7ce01fdb336a7905
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-03-08 21:15:14 +0100

    Updated parser.h

--- src/parser.h
+++ src/parser.h
@@ -3,20 +3,12 @@
 // All original author information and file headers must be preserved.
 // For full license text, see: [https://github.com/JHXStudioriginal/Elasna-License/blob/main/LICENSE]
 
-
 #ifndef PARSER_H
 #define PARSER_H
 
-int split_args(const char *line, char *args[], int max_args);
-void free_args(char *args[], int argc);
-char* unescape_string(const char *s);
-void unescape_args(char *args[], int argc);
-char* expand_tilde(const char *path);
-char* expand_variables(const char *input);
-void replace_alias(char *args[], int *argc);
-void handle_redirection(char *args[], int *argc);
-int process_single_command(char *line);
+#include "ast.h"
+
+ASTNode* parse_ast(const char *line);
 int process_command_line(char *line);
-char* expand_history(const char *line, const char *last_command);
 
 #endif
