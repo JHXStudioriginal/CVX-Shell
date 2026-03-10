325365daa699f4fd9b42455b2fedacb081799146
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-03-10 18:28:31 +0100

    Updated parser.h

--- src/parser.h
+++ src/parser.h
@@ -3,12 +3,28 @@
 // All original author information and file headers must be preserved.
 // For full license text, see: [https://github.com/JHXStudioriginal/Elasna-License/blob/main/LICENSE]
 
-#ifndef PARSER_H
-#define PARSER_H
+#ifndef UTILS_H
+#define UTILS_H
 
-#include "ast.h"
+int split_args(const char *line, char *args[], int max_args);
+void free_args(char *args[], int argc);
+char* unescape_string(const char *s);
+void unescape_args(char *args[], int argc);
+char* expand_tilde(const char *path);
+char* expand_variables(const char *input);
+void replace_alias(char *args[], int *argc);
+void handle_redirection(char *args[], int *argc);
+char* expand_history(const char *line, const char *last_command);
+void expand_glob(char *args[], int *argc, int max_args);
+void quote_removal(char *args[], int argc);
 
-ASTNode* parse_ast(const char *line);
-int process_command_line(char *line);
+typedef struct ParamFrame {
+    int argc;
+    char **argv;
+    struct ParamFrame *next;
+} ParamFrame;
+
+void push_param_frame(int argc, char **argv);
+void pop_param_frame();
 
 #endif
