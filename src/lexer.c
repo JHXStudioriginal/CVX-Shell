fe910f9ec28126d55f7216011b5163e6d1e5cdc1
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-03-10 18:27:01 +0100

    Updated lexer.c

--- src/lexer.c
+++ src/lexer.c
@@ -32,10 +32,12 @@ Token *tokenize(const char *line) {
 
         if (strncmp(p, "&&", 2) == 0) { add_tok(TOK_AND, NULL, 0); p += 2; continue; }
         if (strncmp(p, "||", 2) == 0) { add_tok(TOK_OR, NULL, 0); p += 2; continue; }
+        if (strncmp(p, ";;", 2) == 0) { add_tok(TOK_DSEMI, NULL, 0); p += 2; continue; }
         if (*p == '|') { add_tok(TOK_PIPE, NULL, 0); p++; continue; }
         if (*p == '&') { add_tok(TOK_AMP, NULL, 0); p++; continue; }
         if (*p == '(') { add_tok(TOK_LPAREN, NULL, 0); p++; continue; }
         if (*p == ')') { add_tok(TOK_RPAREN, NULL, 0); p++; continue; }
+        if (*p == '!') { add_tok(TOK_BANG, NULL, 0); p++; continue; }
 
         if (*p == '{') {
             const char *start = p + 1;
@@ -89,21 +91,45 @@ Token *tokenize(const char *line) {
                     in_quotes = true;
                     quote_char = *p;
                 } else if (*p == ' ' || *p == '\t' || *p == '\n' ||
-                           *p == ';' || *p == '|' || *p == '&' ||
+                           *p == ';' || *p == '|' ||
                            *p == '{' || *p == '}' || *p == '(' || *p == ')' ||
                            *p == '#') {
                     break;
+                } else if (*p == '&') {
+                    if (p > start && (*(p-1) == '>' || *(p-1) == '<')) {
+                    } else if (p[1] == '&') {
+                        break;
+                    } else {
+                        break;
+                    }
                 }
             } else {
-                if (*p == quote_char && (p == line || *(p-1) != '\\')) {
-                    in_quotes = false;
+                if (*p == quote_char) {
+                    bool escaped = false;
+                    const char *rev = p - 1;
+                    while (rev >= start && *rev == '\\') {
+                        escaped = !escaped;
+                        rev--;
+                    }
+                    if (!escaped) in_quotes = false;
                 }
             }
             p++;
         }
 
         if (p > start) {
-            add_tok(TOK_STR, start, p - start);
+            char *s = strndup(start, p - start);
+            TokenType t = TOK_STR;
+            if (strcmp(s, "if") == 0) t = TOK_IF;
+            else if (strcmp(s, "then") == 0) t = TOK_THEN;
+            else if (strcmp(s, "else") == 0) t = TOK_ELSE;
+            else if (strcmp(s, "elif") == 0) t = TOK_ELIF;
+            else if (strcmp(s, "fi") == 0) t = TOK_FI;
+            else if (strcmp(s, "case") == 0) t = TOK_CASE;
+            else if (strcmp(s, "in") == 0) t = TOK_IN;
+            else if (strcmp(s, "esac") == 0) t = TOK_ESAC;
+            add_tok(t, s, p - start);
+            free(s);
         } else {
             p++;
         }
@@ -137,20 +163,24 @@ void consume(Token **token) {
 char *concat_tokens(Token *start, Token *end) {
     int len = 0;
     for (Token *t = start; t != end; t = t->next) {
-        if (t->type == TOK_STR || t->type == TOK_BLOCK) {
-            len += strlen(t->val) + 1;
-        }
+        if (t->val) len += strlen(t->val) + 1;
+        else len += 4;
     }
     if (len == 0) return strdup("");
     char *res = malloc(len + 1);
     res[0] = '\0';
     for (Token *t = start; t != end; t = t->next) {
-        if (t->type == TOK_STR || t->type == TOK_BLOCK) {
+        if (t->val) {
             strcat(res, t->val);
-            if (t->next != end && (t->next->type == TOK_STR || t->next->type == TOK_BLOCK)) {
-                strcat(res, " ");
-            }
+        } else {
+            if (t->type == TOK_AND) strcat(res, "&&");
+            else if (t->type == TOK_OR) strcat(res, "||");
+            else if (t->type == TOK_PIPE) strcat(res, "|");
+            else if (t->type == TOK_SEMI) strcat(res, ";");
+            else if (t->type == TOK_AMP) strcat(res, "&");
+            else if (t->type == TOK_DSEMI) strcat(res, ";;");
         }
+        if (t->next != end) strcat(res, " ");
     }
     return res;
 }
@@ -191,8 +221,14 @@ bool is_block_complete(const char *line) {
                 last_op_pos = NULL;
             }
         } else {
-            if (*p == quote_char && (p == line || *(p-1) != '\\')) {
-                in_quotes = false;
+            if (*p == quote_char) {
+                bool escaped = false;
+                const char *rev = p - 1;
+                while (rev >= line && *rev == '\\') {
+                    escaped = !escaped;
+                    rev--;
+                }
+                if (!escaped) in_quotes = false;
             }
         }
         p++;
