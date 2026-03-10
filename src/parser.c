b10b729dec143dc76efd08e1bb69a568184f5a6e
JHX Studio's <121448917+JHXStudioriginal@users.noreply.github.com>
2026-03-10 18:28:16 +0100

    Updated parser.c

--- src/parser.c
+++ src/parser.c
@@ -16,8 +16,82 @@ static ASTNode *parse_command(Token **token);
 static ASTNode *parse_pipeline(Token **token);
 static ASTNode *parse_and_or(Token **token);
 static ASTNode *parse_sequence(Token **token);
+static ASTNode *parse_if(Token **token);
+static ASTNode *parse_case(Token **token);
+
+static ASTNode *parse_if_inner(Token **token, bool expect_fi) {
+    consume(token);
+    ASTNode *cond = parse_sequence(token);
+    if (!match(token, TOK_THEN)) {
+        fprintf(stderr, "syntax error: expected 'then'\n");
+        return NULL;
+    }
+    ASTNode *then_branch = parse_sequence(token);
+    ASTNode *else_branch = NULL;
+    if ((*token)->type == TOK_ELIF) {
+        else_branch = parse_if_inner(token, false);
+    } else if (match(token, TOK_ELSE)) {
+        else_branch = parse_sequence(token);
+    }
+    
+    if (expect_fi && !match(token, TOK_FI)) {
+        fprintf(stderr, "syntax error: expected 'fi'\n");
+    }
+    
+    ASTNode *node = calloc(1, sizeof(*node));
+    node->type = AST_IF;
+    node->cond = cond;
+    node->left = then_branch;
+    node->right = else_branch;
+    return node;
+}
+
+static ASTNode *parse_if(Token **token) {
+    return parse_if_inner(token, true);
+}
+
+static ASTNode *parse_case(Token **token) {
+    consume(token);
+    if ((*token)->type != TOK_STR) return NULL;
+    char *word = strdup((*token)->val);
+    consume(token);
+    if (!match(token, TOK_IN)) {
+        free(word);
+        return NULL;
+    }
+    ASTNode *root = calloc(1, sizeof(*root));
+    root->type = AST_CASE;
+    root->cmd = word;
+    ASTNode **next_item = &root->left;
+
+    while ((*token)->type != TOK_ESAC && (*token)->type != TOK_EOF) {
+        Token *p_start = *token;
+        while ((*token)->type != TOK_RPAREN && (*token)->type != TOK_EOF) {
+            consume(token);
+        }
+        char *pattern = concat_tokens(p_start, *token);
+        consume(token);
+        
+        ASTNode *body = parse_sequence(token);
+        
+        ASTNode *item = calloc(1, sizeof(*item));
+        item->type = AST_CASE_ITEM;
+        item->cmd = pattern;
+        item->left = body;
+        *next_item = item;
+        next_item = &item->right;
+        
+        if ((*token)->type == TOK_DSEMI) consume(token);
+        else if ((*token)->type == TOK_ESAC) break;
+    }
+    match(token, TOK_ESAC);
+    return root;
+}
 
 static ASTNode *parse_command(Token **token) {
+    if ((*token)->type == TOK_IF) return parse_if(token);
+    if ((*token)->type == TOK_CASE) return parse_case(token);
+    
     if ((*token)->type == TOK_STR && 
         (*token)->next && (*token)->next->type == TOK_LPAREN &&
         (*token)->next->next && (*token)->next->next->type == TOK_RPAREN &&
@@ -48,6 +122,11 @@ static ASTNode *parse_command(Token **token) {
 }
 
 static ASTNode *parse_pipeline(Token **token) {
+    bool negate = false;
+    if ((*token)->type == TOK_BANG) {
+        negate = true;
+        consume(token);
+    }
     ASTNode *left = parse_command(token);
     if (!left) return NULL;
     while ((*token)->type == TOK_PIPE) {
@@ -60,6 +139,12 @@ static ASTNode *parse_pipeline(Token **token) {
         node->left = left;
         node->right = right;
         left = node;
+    }
+    if (negate) {
+        ASTNode *neg = calloc(1, sizeof(*neg));
+        neg->type = AST_NEGATION;
+        neg->left = left;
+        left = neg;
     }
     return left;
 }
@@ -89,7 +174,10 @@ static ASTNode *parse_sequence(Token **token) {
     
     ASTNode *left = parse_and_or(token);
     if (!left) {
-        if ((*token)->type != TOK_EOF && (*token)->type != TOK_RPAREN) {
+        if ((*token)->type != TOK_EOF && (*token)->type != TOK_RPAREN &&
+            (*token)->type != TOK_THEN && (*token)->type != TOK_ELIF &&
+            (*token)->type != TOK_ELSE && (*token)->type != TOK_FI &&
+            (*token)->type != TOK_ESAC && (*token)->type != TOK_DSEMI) {
              consume(token);
              return parse_sequence(token);
         }
